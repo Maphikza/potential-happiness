ab0794ec5da483c19d150e6bd07ba6b432d5f17b
Maphikza <stapisi@protonmail.com>
2023-02-16 09:51:07 +0200

    Adding lazy_static and Mutex for global variable.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
+use lazy_static::lazy_static;
 use std::fs::{self, DirEntry};
 use std::io;
 use std::path::Path;
+use std::sync:: Mutex;
+
+// This was a fun little test to improve my Rust programming skills. I have learned quite a bit from this little experiment.
+// This function will print out the total size of the directory path you provide.
+
+lazy_static! {
+    static ref A: Mutex<u64> = Mutex::new(0);
+}
+
 
 fn main() {
     let user_path = get_path();
     let start_path = Path::new(&user_path);
-    println!("The path entered is {}", start_path.is_dir());
-    let total_size = visit_dirs(start_path, &print_entry_size);
-    println!("The total size for this directory is, {:#?}", total_size)
+    let _total_size = visit_dirs(start_path, &print_entry_size);
+    println!("The total size for this directory is, {:#?}", A.lock().unwrap())
 }
 
 fn get_path() -> String {
-    println!("Welcome to file size checker.\n Please enter the directory path.");
+    println!("Welcome to file size checker.\nPlease enter the directory path.");
 
     let mut chosen_path = String::new();
 
@@ -30,16 +39,22 @@ fn get_path() -> String {
 fn print_entry_size(entry: &fs::DirEntry) {
     // println!("{}", entry.path().display());
     let metadata = fs::metadata(entry.path());
-    println!("{:#?}", metadata.expect("This must be a path.").len());
+    let mut mutex_guard = match A.try_lock() {
+        Ok(guard) =>guard,
+        Err(e) => {
+            eprintln!("Failed to acquire lock: {:?}", e);
+            return;
+        }
+    };
+    *mutex_guard += &metadata.expect("This must be a path.").len();
+    // println!("{:#?}", mutex_guard);
 }
 
-fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<u64> {
-    let mut size = 0;
+fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
-            size += fs::metadata(&path).expect("This should be a path.").len();
             if path.is_dir() {
                 visit_dirs(&path, cb)?;
             } else {
@@ -47,5 +62,5 @@ fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<u64> {
             }
         }
     }
-    Ok(size)
+    Ok(())
 }
--- target/debug/deps/rust_files-6afb0e246e44eb7f.d
+++
@@ -1,5 +0,0 @@
-c:\Users\stapi\Siphiwe\projects\rust_files\target\debug\deps\rust_files-6afb0e246e44eb7f.rmeta: src\main.rs
-
-c:\Users\stapi\Siphiwe\projects\rust_files\target\debug\deps\rust_files-6afb0e246e44eb7f.d: src\main.rs
-
-src\main.rs:
